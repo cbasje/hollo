297c82468dd9ac6f92e1671f64a11721bbf1cae2
Sebastiaan Benjamins <sebastiaan@benjami.in>
2024-07-28 17:57:17 +0200

    Move db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,13 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import * as schema from "./schema";
+import * as hunterSchema from "../hunter/schema";
+import * as schema from "../schema";
 
 // biome-ignore lint/complexity/useLiteralKeys: tsc rants about this (TS4111)
 const databaseUrl = process.env["DATABASE_URL"];
 if (databaseUrl == null) throw new Error("DATABASE_URL must be defined");
 
 const client = postgres(databaseUrl, { connect_timeout: 5 });
-export const db = drizzle(client, { schema });
+export const db = drizzle(client, { schema: { ...schema, ...hunterSchema } });
 
 export default db;
