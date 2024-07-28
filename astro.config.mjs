2c83eed8741a328822014ddf532fc677097a9623
Sebastiaan Benjamins <sebastiaan@benjami.in>
2024-07-29 00:05:54 +0200

    Add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,39 @@
 import { defineConfig } from "astro/config";
 
 import hono from "./adapter/index";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: hono(),
+
+  integrations: [
+    icon({
+      include: {
+        "fluent-emoji-high-contrast": [
+          "dashing-away",
+          "boy",
+          "baby",
+          "battery",
+          "eagle",
+          "baby-chick",
+          "stopwatch",
+          "snow-capped-mountain",
+          "girl",
+          "police-car-light",
+        ],
+      },
+      svgoOptions: {
+        plugins: [
+          {
+            name: "convertColors",
+            params: {
+              currentColor: true,
+            },
+          },
+        ],
+      },
+    }),
+  ],
 });
